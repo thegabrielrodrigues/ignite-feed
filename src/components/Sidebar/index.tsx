a3fdaf6e971a73f70f258b65cf365f40a7f4332c
thegabrielrodrigues <gabrielrofreitas.dev@gmail.com>
2023-05-22 19:04:15 -0300

    sidebar da página

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,28 @@
+import { PencilLine } from '@phosphor-icons/react';
+
+import styles from './styles.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=100"
+      />
+
+      <div className={styles.user_info}>
+        <img className={styles.avatar} src="https://github.com/thegabrielrodrigues.png" />
+
+        <strong>Gabriel Rodrigues</strong>
+        <span>Web Developer</span>
+
+        <div className={styles.edit_profile}>
+          <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil
+          </a>
+        </div>
+      </div>
+    </aside>
+  );
+}
